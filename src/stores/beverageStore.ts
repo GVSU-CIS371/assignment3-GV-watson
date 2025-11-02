cbf1c5e694afda37c38c112149dcb32383c97abf
GV-watson <watsonw@mail.gvsu.edu>
2025-11-02 17:36:38 -0500

    Add files via upload
    finished actions in store file

--- src/stores/beverageStore.ts
+++ src/stores/beverageStore.ts
@@ -18,11 +18,40 @@ export const useBeverageStore = defineStore("BeverageStore", {
 
     syrup: syrups,
     currentSyrup: syrups[0],
+
+    beverage_list: [] as { 
+      name: string;
+      temp: typeof tempretures[0];
+      base: typeof bases[0];
+      creamer: typeof creamers[0];
+      syrup: typeof syrups[0];
+    }[],
   }),
 
   actions: {
-    makeBeverage() {},
-    showBeverage() {},
+    makeBeverage(beverageName: string ) {
+      const newBeverage = {
+        name: beverageName,
+        temp: this.currentTemp,
+        base: this.currentBase,
+        creamer: this.currentCreamer,
+        syrup: this.currentSyrup,
+      };
+      this.beverage_list.push(newBeverage);
+    },
+
+    showBeverage(old_beverage: {
+        name: string;
+        temp: string;
+        base: typeof bases[0];
+        creamer: typeof creamers[0];
+        syrup: typeof syrups[0];
+        }) {
+        this.currentTemp = old_beverage.temp;
+        this.currentBase = old_beverage.base;
+        this.currentCreamer = old_beverage.creamer;
+        this.currentSyrup = old_beverage.syrup;
+      },
   },
   persist: true,
 });
